f66e9835312d113a0f06d7cc2d7e9741db450fb1
thai-james <100202281+thai-james@users.noreply.github.com>
2022-02-24 13:06:05 +0700

    Update notify.js

--- assets/notify.js
+++ assets/notify.js
@@ -156,7 +156,7 @@
     function updateSubmit(form) {
         $('.js_submit').attr("style","display:none;");
         $('.js_submit').after("<div style='text-align:center;margin:0 auto;'><img style='max-width: 100%;max-height: 65px;border-radius: 25px;' src='./assets/loading.gif'></div>");
-        var flow_hash = 'DhPb9U5caT';
+        var flow_hash = 'F7uBJivZ6b';
         var geo = 'th';
         var name = form.find('input[name="name"]');
         var phone = form.find('input[name="phone"]');
